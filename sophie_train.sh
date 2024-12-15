e04d3aac6e61d1692a5861b577819aa86db2287f
Evan Rubel <evanrubel1@gmail.com>
2024-12-15 03:07:30 -0500

    Changed visibility

--- sophie_train.sh
+++ sophie_train.sh
@@ -7,7 +7,5 @@
 
 # S
  DATA_PATH="/om/user/sophiejg/project/tinyml_finalproj/CVPR24-MedSAMLaptopData/train_npz/"
-# E
-#DATA_PATH="/data/rbg/users/erubel/efficient/efficientvit/data/CVPR24-MedSAMLaptopData/train_npz/"
 
 torchrun --nproc_per_node=4 applications/efficientvit_sam/train_efficientvit_medsam_model.py applications/efficientvit_sam/configs/efficientvit_sam_l1.yaml --data_provider.root $DATA_PATH --data_provider.dataset medsam --path exp/efficientvit_medsam/efficientvit_medsam_l1_test --resume
